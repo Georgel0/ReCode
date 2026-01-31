1ded24c56f602638a8a4c92180984e3063204a87
Georgel0 <georgelgarabajiu07@gmail.com>
2026-01-31 14:17:38 +0200

    update

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -68,7 +68,7 @@ export default function MainLayout({ children }) {
                     <span style={{ color: '#fff' }}>ReCode</span>
                   </div>
                 </Link>
-                <Link href="/code-converter" className="primary-button" style={{ padding: '6px 12px', fontSize: '0.85rem' }}>
+                <Link href="/code-converter" className="primary-button launch-app-btn">
                   Launch App
                 </Link>
              </>
